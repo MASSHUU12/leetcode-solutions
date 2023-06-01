3865bb654c195457b4ac8b896f9877dcd9f7c51f
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-06-01 16:11:13 +0200

    Solved 1470. Shuffle the Array TS

---
+++ challenges/1470_ShuffleTheArray/main.ts
@@ -0,0 +1,11 @@
+function shuffle(nums: number[], n: number): number[] {
+  const result: number[] = [];
+
+  for (let i = 0; i < n; i++) {
+    result.push(nums[i], nums[i + n]);
+  }
+
+  return result;
+}
+
+console.log(shuffle([2, 5, 1, 3, 4, 7], 3));

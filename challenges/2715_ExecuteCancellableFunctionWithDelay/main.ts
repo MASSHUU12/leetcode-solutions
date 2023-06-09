eb66893c0330809a089eaf7978b0e8c2d3d1e7d4
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-06-09 20:17:36 +0200

    Solved 2725. Interval Cancellation

--- challenges/2715_ExecuteCancellableFunctionWithDelay/main.ts
+++ challenges/2715_ExecuteCancellableFunctionWithDelay/main.ts
@@ -1,4 +1,4 @@
-function cancellable(fn: Function, args: unknown[], t: number): Function {
+function _cancellable(fn: Function, args: unknown[], t: number): Function {
   const timeoutId = setTimeout(() => {
     fn.apply(null, args);
   }, t);
@@ -7,17 +7,3 @@ function cancellable(fn: Function, args: unknown[], t: number): Function {
     clearTimeout(timeoutId);
   };
 }
-
-const result: number[] = [];
-
-const fn = (x: number) => x * 5;
-const args = [2],
-  t = 20,
-  cancelT = 50;
-
-const cancel = cancellable(fn, args, t);
-
-setTimeout(() => {
-  cancel();
-  console.log(result); // [{"time":20,"returned":10}]
-}, cancelT);

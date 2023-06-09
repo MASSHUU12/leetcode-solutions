6e98e0f94ca87517b64fcdcacddf029cc3eb650c
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-06-09 19:45:43 +0200

    Solved 2723. Add Two Promises

---
+++ challenges/2723_AddTwoPromises/main.ts
@@ -0,0 +1,10 @@
+function addTwoPromises(
+  promise1: Promise<number>,
+  promise2: Promise<number>
+): Promise<number> {
+  return Promise.all([promise1, promise2]).then(([result1, result2]) => {
+    return Promise.resolve(result1 + result2);
+  });
+}
+
+addTwoPromises(Promise.resolve(2), Promise.resolve(2)).then(console.log); // 4

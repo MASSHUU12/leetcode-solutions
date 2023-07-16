299721eb90573ef71f75fe80a917d255fc6c375e
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-07-16 22:02:31 +0200

    Solved 2574. Left and Right Sum Differences

--- challenges/2574_LeftAndRightSumDifferences/main.ts
+++ challenges/2574_LeftAndRightSumDifferences/main.ts
@@ -1,21 +1,16 @@
 function leftRightDifference(nums: number[]): number[] {
   const len = nums.length;
-  const leftSum: number[] = Array(len).fill(0);
-  const rightSum: number[] = Array(len).fill(0);
-  const result: number[] = [];
+  const result: number[] = new Array(len);
 
-  for (let i = 1; i < len; i++) {
-    leftSum[i] = nums[i - 1] + leftSum[i - 1];
-  }
+  let totalSum = 0;
+  let leftSum = 0;
 
-  for (let i = len - 1; i >= 0; i--) {
-    if (i === len - 1) continue;
-
-    rightSum[i] = nums[i + 1] + rightSum[i + 1];
-  }
+  for (let i = 0; i < len; i++) totalSum += nums[i];
 
   for (let i = 0; i < len; i++) {
-    result.push(Math.abs(leftSum[i] - rightSum[i]));
+    totalSum -= nums[i];
+    result[i] = Math.abs(leftSum - totalSum);
+    leftSum += nums[i];
   }
 
   return result;

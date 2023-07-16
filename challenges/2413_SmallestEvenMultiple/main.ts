52a01982c73bd9d0878390dc11ad07b43a1dd8aa
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-07-16 20:52:46 +0200

    Solved 2413. Smallest Even Multiple

---
+++ challenges/2413_SmallestEvenMultiple/main.ts
@@ -0,0 +1,10 @@
+function smallestEvenMultiple(n: number): number {
+  for (let i = 1; i < Infinity; i++) {
+    if (i % 2 === 0 && i % n === 0) return i;
+  }
+
+  return -1;
+}
+
+console.log(smallestEvenMultiple(5)); // Should be 10
+console.log(smallestEvenMultiple(6)); // Should be 6
